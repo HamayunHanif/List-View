d7b262197a914cc2d2d8dbadb37c06d80a8175a0
Hamayunhanif <hamayunhanif121@gmail.com>
2022-02-21 07:41:15 -0800

    Card View (Recycle View)

--- app/src/main/java/com/example/cardview/MainActivity.java
+++ app/src/main/java/com/example/cardview/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,17 +18,26 @@ public class MainActivity extends AppCompatActivity {
     private int[]  images ={R.drawable.jh,R.drawable.huzi,R.drawable.zuhaib,R.drawable.moeed};
     private List<persons> personsList = new ArrayList<>();
     private RecyclerView recyclerView;
+    FloatingActionButton btnOpendialoge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.rvw);
+        btnOpendialoge = findViewById(R.id.btnOpendialoge);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         preparetheList();
 
         RecyclerAdapter adapter = new RecyclerAdapter(personsList);
         recyclerView.setAdapter(adapter);
+
+        btnOpendialoge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
     private void  preparetheList(){
         int count = 0;
